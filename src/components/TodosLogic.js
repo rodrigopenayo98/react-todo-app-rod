b7866d83dabedc94aa694d2e4a1c73d1ded1550f
rod <91923422+rodrigopenayo98@users.noreply.github.com>
2023-07-18 00:29:34 -0300

    add new items and error message

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,21 +1,22 @@
 import InputTodo from './InputTodo.js';
 import TodosList from './TodoList.js';
 import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: 'Setup development environment',
       completed: true,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: 'Develop website and add content',
       completed: false,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: 'Deploy to live server',
       completed: false,
     },
@@ -29,9 +30,18 @@ const TodosLogic = () => {
     ]);
   };
 
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div>
-      <InputTodo />
+      <InputTodo addTodoItem={addTodoItem} />
       <TodosList todosProps={todos} setTodos={setTodos} delTodo={delTodo}/>
     </div>
   );
